50cee3c695f9df9d93a64b61052647f2f8007026
Superdave56 <dvnguyen56@gmail.com>
2017-09-04 15:05:25 -0500

    beatbox cont

--- Beatbox/MiniMusicCmdLine.java
+++ Beatbox/MiniMusicCmdLine.java
@@ -1,12 +1,18 @@
 import javax.sound.midi.*;
 
-public class MiniMiniMusicApp {
+public class MiniMusicCmdLine {
 	public static void main (String[] args) {
-		MiniMiniMusicApp mini = new MiniMiniMusicApp();
-		mini.play();
+		MiniMusicCmdLine mini = new MiniMusicCmdLine();
+		if(args.length < 2) {
+			System.out.println("Don't forget the instrument and the note.");
+		} else {
+			int instrument = Integer.parseInt(args[0]);
+			int note = Integer.parseInt(args[1]);
+			mini.play(instrument, note);
+		}
 	} // close main
 	
-	public void play() {
+	public void play(int instrument, int note) {
 		try {
 			Sequencer player = MidiSystem.getSequencer(); // get Sequencer
 			player.open(); // open Sequencer
@@ -15,19 +21,21 @@ public class MiniMiniMusicApp {
 			
 			Track track = seq.createTrack(); // ask Sequence for a Track
 			
+			MidiEvent event = null;
+			
 			// put MidiEvents into the Track
 			ShortMessage first = new ShortMessage();
-			first.setMessage(192, 1, 113, 0);
+			first.setMessage(192, 1, instrument, 0);
 			MidiEvent changeInstrument = new MidiEvent(first, 1);
 			track.add(changeInstrument);
 			
 			ShortMessage a = new ShortMessage();
-			a.setMessage(144, 1, 20, 100);
+			a.setMessage(144, 1, note, 100);
 			MidiEvent noteOn = new MidiEvent(a, 1);
 			track.add(noteOn);
 			
 			ShortMessage b = new ShortMessage();
-			b.setMessage(128, 1, 20, 100);
+			b.setMessage(128, 1, note, 100);
 			MidiEvent noteOff = new MidiEvent(b, 3);
 			track.add(noteOff);
 			
